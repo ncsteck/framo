c2690de44401b18667a10f0056b30a7e8fcc666f
Roman Steck <roman.steck@netcase.ch>
2014-05-16 06:39:11 -0500

    QR barcodescanner

--- ch.netcase.framo/scripts/main.js
+++ ch.netcase.framo/scripts/main.js
@@ -1,51 +1,95 @@
-document.addEventListener("deviceready", onDeviceReady, false);
-
-function onDeviceReady() {
-    navigator.splashscreen.hide();
-    var app = new App();
-    app.run();
-}
-
-function App() {
-}
-
-App.prototype = {
-    resultsField: null,
-     
-    run: function() {
-        var that = this,
-        scanButton = document.getElementById("scanButton");
-        
-        that.resultsField = document.getElementById("result");
-        
-        scanButton.addEventListener("click",
-                                    function() { 
-                                        that._scan.call(that); 
-                                    });
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
     },
-    
-    _scan: function() {
-        var that = this;
-        if (window.navigator.simulator === true) {
-            alert("Not Supported in Simulator.");
-        }
-        else {
-            cordova.plugins.barcodeScanner.scan(
-                function(result) {
-                    if (!result.cancelled) {
-                        that._addMessageToLog(result.format + " | " + result.text);    
-                    }
-                }, 
-                function(error) {
-                    console.log("Scanning failed: " + error);
-                });
-        }
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // `load`, `deviceready`, `offline`, and `online`.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.getElementById('scan').addEventListener('click', this.scan, false);
+        document.getElementById('encode').addEventListener('click', this.encode, false);
     },
 
-    _addMessageToLog: function(message) {
-        var that = this,
-        currentMessage = that.resultsField.innerHTML;
+    // deviceready Event Handler
+    //
+    // The scope of `this` is the event. In order to call the `receivedEvent`
+    // function, we must explicity call `app.receivedEvent(...);`
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    },
+
+    scan: function() {
+        console.log('scanning');
         
-        that.resultsField.innerHTML = currentMessage + message + '<br />'; 
+        var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+
+        scanner.scan( function (result) { 
+			
+            /*alert("We got a barcode\n" + 
+            "Result: " + result.text + "\n" + 
+            "Format: " + result.format + "\n" + 
+            "Cancelled: " + result.cancelled);  
+			*/
+           console.log("Scanner result: \n" +
+                "text: " + result.text + "\n" +
+                "format: " + result.format + "\n" +
+                "cancelled: " + result.cancelled + "\n");
+            document.getElementById("info").innerHTML = result.text;
+            console.log(result);
+            /*
+            if (args.format == "QR_CODE") {
+                window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
+            }
+            */
+
+        }, function (error) { 
+            console.log("Scanning failed: ", error); 
+        } );
+    },
+
+    encode: function() {
+        var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+
+        scanner.encode(scanner.Encode.TEXT_TYPE, "http://www.nhl.com", function(success) {
+            alert("encode success: " + success);
+          }, function(fail) {
+            alert("encoding failed: " + fail);
+          }
+        );
+
     }
-}
+
+};
